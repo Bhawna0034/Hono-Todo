4ce0e0b3f7975f97044dac5bc9d90680912c745a
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-30 16:33:14 +0530

    Grouping without changing a base & BasePath

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,36 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { basicAuth } from "hono/basic-auth";
 
+// Grouping
+// const book = new Hono();
+// book.get('/', (c) => c.text('List Books'))
+// book.get('/:id', (c) => {
+//    const id = c.req.param('id');
+//    return c.text(`Get Book: ${id}`);
+// })
+// book.post('/', (c) => c.text('Create Book'));
+
+// const app = new Hono();
+// app.route('/book', book);
+
+// Grouping without changing a base
+const book = new Hono();
+book.get('/book', (c) => c.text('List Books'))
+book.post('/book', (c) => c.text('Create Book'));
+
+const user = new Hono().basePath('/user')
+user.get('/', (c) => c.text('List Users'))
+user.post('/', (c) => c.text('Create User'))
 const app = new Hono();
+app.route('/', book);
+app.route('/', user);
 
-// Any HTTP Method
-app.all('/hello', (c) => {
- return c.text('Any Method, /hello')
-})
 
-// Multiple Method
-app.on(['PUT', 'DELETE'], '/posts', (c) => {
-   return c.text('PUT or Delete /posts');
-})
+// BasePath
+const api = new Hono().basePath('/api')
+api.get('/books', (c) => c.text('List Books')); // Get api/book
 
-// Multiple Paths
-app.on('GET', ['/hello', '/foo/hello', '/boo/hello'], (c) => {
-  return c.text('Hello Multiple Paths');
-})
-
-// Path Parameter
-app.get('/profile/:username', (c) => {
-  const username = c.req.param('username');
-  return c.text(`Profile: ${username}`);
-})
-
-app.get('/profile/:id/comment/:comment_id', (c) => {
-  const {id, comment_id} = c.req.param();
-  return c.text(`Id: ${id}, Comment Id: ${comment_id}`);
-})
-
-// Optional Paramter
-app.get('/profile/:id/:bio?', (c) => {
-  const {id, bio} = c.req.param();
-  return c.text(`Id: ${id} ${bio}`);
-})
-
-// RegExp
-app.get('/profile/:id{[0-9]+}/:username{[a-z]+}', (c) => {
-  const {id, username} = c.req.param();
-  return c.text(`${id} ${username}`);
-})
-
-// Chained route
-app
-   .get('/instagram', (c) => {
-      return c.text(`GET /instagram`)
-   })
-   .post((c) => {
-      return c.text('POST /instagram')
-   })
-   .put((c) => {
-      return c.text('PUT /instagram')
-   })
-   .delete((c) => {
-      return c.text('DELETE /instagram')
-   })
+app.route('/', api);
 
 serve({
   fetch: app.fetch,
