d7b2cc96f7284d1e68feb03f715320e7bdd7b78a
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-05-03 15:53:33 +0530

    Handling Protected Routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-import { Hono } from "hono";
+import dotenv from "dotenv";
+import { Hono, type Next } from "hono";
 import { serve } from "@hono/node-server";
 import { HTTPException } from "hono/http-exception";
+import { logger } from 'hono/logger'
+import jwt from 'jsonwebtoken'
 import type { Todo, User } from "./types.js";
+import type { Context } from "hono";
+import { auth } from "hono/utils/basic-auth";
 
 const app = new Hono();
 
 let Todos: Todo[] = [];
 let Users: User[] = [];
 
+dotenv.config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
+if (!JWT_SECRET_KEY) {
+  throw new Error("JWT_SECRET is not defined in .env");
+}
+app.use(logger()); 
 // Signup
 app.post("/signup", async (c) => {
   const { email, password } = await c.req.json();
@@ -37,22 +48,52 @@ app.post("/login", async(c) => {
   if(!user){
     throw new HTTPException(401, {message: "Invalid email and password"});
   }
-  const passwordMatched = Users.find((user) => user.password === password);
+  const passwordMatched = user.password === password;
   if(!passwordMatched){
     throw new HTTPException(401, {message: "Password Do not Match!"});
   }
+  
+
+
+  const token = jwt.sign({id: user.id, email: user.email}, JWT_SECRET_KEY, {
+    expiresIn: "1h",
+  })
+
 
   return c.json({
     message: "Login Successful!",
-    email: user.email
+    token
   });
 });
 
+// Authentication
+const authMiddleware = async(c: Context, next: Next) => {
+  const authHeader = c.req.header("Authorization");
+  if(!authHeader || !authHeader.startsWith("Bearer ")){
+    throw new HTTPException(401, {message: "Missing or Malformed Token"});
+  }
+
+  const token = authHeader.split(" ")[1];
+  try{
+    const decoded = jwt.verify(token, JWT_SECRET_KEY);
+    c.set("user", decoded);
+    await next();
+  }catch{
+    throw new HTTPException(401, {message: 
+      "Invalid or expired token"
+    });
+  }
+
+}
 // Get All Todo
-app.get("/todos", (c) => c.json(Todos));
+app.get("/todos", authMiddleware, (c) => {
+  return c.json(Todos)
+}
+
+);
 
 // Create Todo
-app.post("/todos", async (c) => {
+app.post("/todos", authMiddleware, async (c) => {
   const body = await c.req.json();
   if (!body.title) {
     throw new HTTPException(400, { message: "Task is required" });
@@ -68,7 +109,7 @@ app.post("/todos", async (c) => {
 });
 
 // Get Todo By Id
-app.get("/todos/:id", (c) => {
+app.get("/todos/:id", authMiddleware, (c) => {
   const id = Number(c.req.param("id"));
   const todoById = Todos.find((todo) => todo.id === id);
   if (!todoById) {
@@ -83,7 +124,7 @@ app.get("/todos/:id", (c) => {
 });
 
 // Delete Todo By Id
-app.delete("/todos/:id", (c) => {
+app.delete("/todos/:id", authMiddleware, (c) => {
   const id = Number(c.req.param("id"));
   const todoExists = Todos.some((todo) => todo.id === id);
   if (!todoExists) {
@@ -101,13 +142,13 @@ app.delete("/todos/:id", (c) => {
 });
 
 // Delete Todo
-app.delete("/todos", (c) => {
+app.delete("/todos", authMiddleware, (c) => {
   Todos = [];
   return c.json("All Todos have been deleted successfully!");
 });
 
 // Edit Todo By Id
-app.put("/todos/:id", async (c) => {
+app.put("/todos/:id", authMiddleware, async (c) => {
   const id = Number(c.req.param("id"));
   const body = await c.req.json();
   const TodoById = Todos.find((todo) => todo.id === id);
